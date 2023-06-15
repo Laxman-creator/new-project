5e5c9da32cbcd06fd6cd8acc7a8fef671d196508
Laxman Singh <laxupipaliya1999@gmail.com>
2023-06-15 14:14:01 +0530

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+
+    
+    <div className='header'>
+
+    <h1>
+      Movies Adda
+    </h1>
+    </div>
+  )
+}
+
+export default Header
